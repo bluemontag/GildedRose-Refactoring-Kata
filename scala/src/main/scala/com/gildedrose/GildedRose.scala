723b85d18430d543d1108ce188abe13b62101856
Ignacio Gallego Sagastume <igallegosagastume@gmail.com>
2020-12-13 20:43:10 +0100

    Refactor 2: Replace double nested if statements by an if with an AND condition

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -11,7 +11,7 @@ class GildedRose(val items: Array[Item]) {
   }
 
   def updateItem(item: Item) {
-          if (!item.name.equals("Aged Brie")
+      if (!item.name.equals("Aged Brie")
         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (item.quality > 0) {
           if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -23,16 +23,11 @@ class GildedRose(val items: Array[Item]) {
           item.quality = item.quality + 1
 
           if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
+            if (item.sellIn < 11 && item.quality < 50) {
                 item.quality = item.quality + 1
-              }
             }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
+            if (item.sellIn < 6 && item.quality < 50) {
                 item.quality = item.quality + 1
-              }
             }
           }
         }
@@ -45,10 +40,8 @@ class GildedRose(val items: Array[Item]) {
       if (item.sellIn < 0) {
         if (!item.name.equals("Aged Brie")) {
           if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.quality = item.quality - 1
-              }
             }
           } else {
             item.quality = item.quality - item.quality
