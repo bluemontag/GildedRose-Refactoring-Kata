56c8d297682daa211de6efba3eb77cb251993b3a
Ignacio Gallego Sagastume <igallegosagastume@gmail.com>
2020-12-13 21:11:07 +0100

    Refactor 4: Change negative logic by positive, inverting blocks IF and ELSE when possible. Comment code and improve method name.

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,17 +6,13 @@ class GildedRose(val items: Array[Item]) {
   def updateQuality() {
     for (i <- 0 until items.length) {
 
-      updateItem(items(i))
+      updateItemQuality(items(i))
     }
   }
 
-  def updateItem(item: Item) {
-    if (!isAgedBrieItem(item)
-      && !isBackstagePassesItem(item)) {
-      if (item.quality > 0 && !isSulfurasItem(item)) {
-          item.quality = item.quality - 1
-      }
-    } else {
+  def updateItemQuality(item: Item) {
+    if (isAgedBrieItem(item) || isBackstagePassesItem(item)) {
+      // is special item
       if (item.quality < 50) {
         item.quality = item.quality + 1
 
@@ -28,25 +24,39 @@ class GildedRose(val items: Array[Item]) {
               item.quality = item.quality + 1
           }
         }
+      }      
+    } else {
+      // is standard item here
+      if (item.quality > 0 && !isSulfurasItem(item)) {
+          // is not sulfuras
+          item.quality = item.quality - 1
       }
     }
 
+    // update the sell by date, but not for sulfuras (never to be sold)
     if (!isSulfurasItem(item)) {
       item.sellIn = item.sellIn - 1
     }
 
+    // update quality for sold items
     if (item.sellIn < 0) {
-      if (!isAgedBrieItem(item)) {
-        if (!isBackstagePassesItem(item)) {
-          if (item.quality > 0 && !isSulfurasItem(item)) {
-              item.quality = item.quality - 1
-          }
-        } else {
-          item.quality = 0
-        }
-      } else {
+      if (isAgedBrieItem(item)) {
+        // it is aged brie
         if (item.quality < 50) {
           item.quality = item.quality + 1
+        }
+      } else {
+        // if it is a concert ticket
+        if (isBackstagePassesItem(item)) {
+          // ticket no longer valid, quality is zero
+          item.quality = 0
+        } else {
+          // is not a concert pass ticket
+          // for any other items except sulfuras
+          if (item.quality > 0 && !isSulfurasItem(item)) {
+              // quality degrades by 1
+              item.quality = item.quality - 1
+          }
         }
       }
     }
